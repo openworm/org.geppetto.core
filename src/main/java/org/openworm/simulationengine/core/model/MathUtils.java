f03b484e20fb5eceafb0dbc72da53bc6000d8ef6
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-06-08 18:22:28 +0200

    Added MathUtils

---
+++ src/main/java/org/openworm/simulationengine/core/model/MathUtils.java
@@ -0,0 +1,14 @@
+package org.openworm.simulationengine.core.model;
+
+import java.util.Random;
+
+public class MathUtils {
+
+	
+	public static float scale(float min, float max, float r){
+		return (float)(min + r * (max - min));
+	}
+	
+	public static Random randomGenerator = new Random();
+	
+}
