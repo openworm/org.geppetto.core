1cdf6eb1f58f9d1a43643db1ba29f8af1be6bba1
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-10-30 15:08:21 +0000

    Added interface to create visualisation model

--- src/main/java/org/openworm/simulationengine/core/model/IModelInterpreter.java
+++ src/main/java/org/openworm/simulationengine/core/model/IModelInterpreter.java
@@ -3,8 +3,12 @@ package org.openworm.simulationengine.core.model;
 import java.net.URL;
 import java.util.List;
 
+import org.openworm.simulationengine.core.visualisation.model.Scene;
+
 public interface IModelInterpreter {
 
 	List<IModel> readModel(URL url);
+	
+	Scene getSceneFromModel(List<IModel> model);
 	
 }
