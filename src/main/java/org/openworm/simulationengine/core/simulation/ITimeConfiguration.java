198a2a93d73f704929a7d08bb8aa12d2a036810d
Giovanni Idili <giovanni.idili@gmail.com>
2013-02-10 23:16:45 +0000

    Update src/main/java/org/openworm/simulationengine/core/simulation/ITimeConfiguration.java
    improved comments

--- src/main/java/org/openworm/simulationengine/core/simulation/ITimeConfiguration.java
+++ src/main/java/org/openworm/simulationengine/core/simulation/ITimeConfiguration.java
@@ -4,19 +4,22 @@ public interface ITimeConfiguration {
 
 	
 	/**
-	 * @return how long is one time step
+	 * @return time step
+	 * NOTE: this is ms
+	 * TODO: need a way to store the unit of measurement
 	 */
 	Float getTimeStepLength();
 	
 	/**
-	 * @return for how many time steps a operation will have to be done
+	 * @return number of time steps to run
 	 */
 	Integer getTimeSteps();
 	
 	/**
-	 * @return the simple period of the time steps. Has to be a divider of time steps.
+	 * @return the sample period of the time steps. Has to be a divider of time steps.
 	 * This will specify the resolution of returned partial results. 
-	 * E.g. TimeSteps=500, SamplePeriod=100 => 5 results are returned
+	 * Examples: 
+	 * TimeSteps=500, SamplePeriod=100 => 5 results are returned
 	 * TimeSteps=500, SamplePeriod=1 => all 500 results returned
 	 * TimeSteps=500, SamplePeriod=500 => only last result returned
 	 */
