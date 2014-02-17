f3dc69796a9cc3fd816995cbf0276a37d24dac85
jrmartin <jesus@metacell.us>
2014-02-16 21:14:10 -0800

    Adding missing comments part to new classes for scope

--- src/main/java/org/geppetto/core/scope/ThreadScope.java
+++ src/main/java/org/geppetto/core/scope/ThreadScope.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.geppetto.core.scope;
 
 import java.util.HashMap;
@@ -7,8 +22,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
-import org.springframework.context.support.SimpleThreadScope;
-import org.springframework.core.NamedInheritableThreadLocal;
 
 /**
  * Thread scope implementation.
