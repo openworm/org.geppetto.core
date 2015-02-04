75991c3f03cc617b2131e58a766da8b2bf07453b
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-02-04 15:23:04 +0000

    Faster URL reader

--- src/main/java/org/geppetto/core/utilities/URLReader.java
+++ src/main/java/org/geppetto/core/utilities/URLReader.java
@@ -3,9 +3,11 @@
  */
 package org.geppetto.core.utilities;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Scanner;
+import java.net.URLConnection;
 
 /**
  * @author matteocantarelli
@@ -13,12 +15,36 @@ import java.util.Scanner;
  */
 public class URLReader
 {
-	
+
 	public static String readStringFromURL(URL url) throws IOException
 	{
-		Scanner s = new Scanner(url.openStream(), "UTF-8");
-		String content = s.useDelimiter("\\A").next();
-		s.close();
-		return content;
+		StringBuilder content = new StringBuilder();
+
+		// many of these calls can throw exceptions, so i've just
+		// wrapped them all in one try/catch statement.
+		try
+		{
+
+			// create a urlconnection object
+			URLConnection urlConnection = url.openConnection();
+
+			// wrap the urlconnection in a bufferedreader
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+			String line;
+
+			// read from the urlconnection via the bufferedreader
+			while((line = bufferedReader.readLine()) != null)
+			{
+				content.append(line + "\n");
+			}
+			bufferedReader.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return content.toString();
+
 	}
 }
