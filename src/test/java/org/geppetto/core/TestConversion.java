cad041d7b63a04a97fc49b3d4cd11b211235f278
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-03-10 18:00:15 +0000

    some classes missing from previous commit

---
+++ src/test/java/org/geppetto/core/TestConversion.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011, 2013 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+package org.geppetto.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.geppetto.core.conversion.ConversionException;
+import org.geppetto.core.conversion.IConversion;
+import org.geppetto.core.model.IModel;
+import org.geppetto.core.services.IModelFormat;
+import org.geppetto.core.services.registry.ServicesRegistry;
+
+/**
+ * @author Adrian Quintana (adrian.perez@ucl.ac.uk)
+ * 
+ */
+public class TestConversion implements IConversion
+{
+
+	@Override
+	public void registerGeppettoService()
+	{
+		ServicesRegistry.registerConversionService(this, getSupportedInputs(), getSupportedOutputs());
+	}
+
+	@Override
+	public List<IModelFormat> getSupportedOutputs(IModel model, IModelFormat input) throws ConversionException
+	{
+		List<IModelFormat> modelFormatList = new ArrayList<IModelFormat>();
+		modelFormatList.add(ModelFormat.TEST2);
+		return modelFormatList;
+	}
+
+	@Override
+	public List<IModelFormat> getSupportedOutputs()
+	{
+		List<IModelFormat> modelFormatList = new ArrayList<IModelFormat>();
+		modelFormatList.add(ModelFormat.TEST2);
+		return modelFormatList;
+	}
+
+	@Override
+	public List<IModelFormat> getSupportedInputs()
+	{
+		List<IModelFormat> modelFormatList = new ArrayList<IModelFormat>();
+		modelFormatList.add(ModelFormat.TEST);
+		return modelFormatList;
+	}
+
+	@Override
+	public void setSupportedInputs(List<IModelFormat> supportedInputs) throws ConversionException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addSupportedInput(IModelFormat supportedInput) throws ConversionException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkSupportedFormat(IModelFormat input) throws ConversionException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IModel convert(IModel model, IModelFormat input, IModelFormat output) throws ConversionException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/org/geppetto/core/TestSimulator2.java
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011 - 2015 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+package org.geppetto.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.geppetto.core.common.GeppettoExecutionException;
+import org.geppetto.core.common.GeppettoInitializationException;
+import org.geppetto.core.data.model.VariableList;
+import org.geppetto.core.model.IModel;
+import org.geppetto.core.model.ModelInterpreterException;
+import org.geppetto.core.model.runtime.AspectNode;
+import org.geppetto.core.services.IModelFormat;
+import org.geppetto.core.services.registry.ServicesRegistry;
+import org.geppetto.core.simulation.IRunConfiguration;
+import org.geppetto.core.simulation.ISimulatorCallbackListener;
+import org.geppetto.core.simulator.ISimulator;
+
+/**
+ * Dummy simulator used for testing purposes 
+ * 
+ * @author Adrian Quintana (adrian.perez@ucl.ac.uk)
+ *
+ */
+public class TestSimulator2 implements ISimulator
+{
+
+	@Override
+	public void simulate(IRunConfiguration runConfiguration, AspectNode aspect) throws GeppettoExecutionException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(List<IModel> models, ISimulatorCallbackListener listener) throws GeppettoInitializationException, GeppettoExecutionException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isInitialized()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public VariableList getForceableVariables()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VariableList getWatchableVariables()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return "test";
+	}
+
+	@Override
+	public void addWatchVariables(List<String> variableNames)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startWatch()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopWatch()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearWatchVariables()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean populateVisualTree(AspectNode aspectNode) throws ModelInterpreterException
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getId()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInitialized(boolean initialized) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getTimeStepUnit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void resetWatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerGeppettoService()
+	{
+		List<IModelFormat> modelFormatList = new ArrayList<IModelFormat>();
+		modelFormatList.add(ModelFormat.TEST2);
+		ServicesRegistry.registerSimulatorService(this, modelFormatList);
+		
+	}
+
+}
