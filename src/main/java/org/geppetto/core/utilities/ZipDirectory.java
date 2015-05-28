8869132b9dc271040cc9bbf24c30ad03a9527269
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-05-28 18:02:51 +0100

    add traces, remove system.out and polish code

--- src/main/java/org/geppetto/core/utilities/ZipDirectory.java
+++ src/main/java/org/geppetto/core/utilities/ZipDirectory.java
@@ -12,73 +12,85 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class ZipDirectory {
-	
-	public static Path getZipFromDirectory(File sourceFolder){
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class ZipDirectory
+{
+
+	private static Log logger = LogFactory.getLog(ZipDirectory.class);
+
+	public static Path getZipFromDirectory(File sourceFolder)
+	{
 		return getZipFromDirectory(sourceFolder, sourceFolder.getName() + ".zip");
 	}
-	
-	public static Path getZipFromDirectory(File sourceFolder, String outputFileName){
+
+	public static Path getZipFromDirectory(File sourceFolder, String outputFileName)
+	{
 		List<File> fileList = new ArrayList<File>();
 		getAllFiles(sourceFolder, fileList);
 		writeZipFile(sourceFolder, outputFileName, fileList);
-		return Paths.get(sourceFolder.getAbsolutePath()  + System.getProperty("file.separator") + outputFileName);
-		
+		return Paths.get(sourceFolder.getAbsolutePath() + System.getProperty("file.separator") + outputFileName);
+
 	}
 
-	public static void getAllFiles(File dir, List<File> fileList) {
-		try {
-			File[] files = dir.listFiles();
-			for (File file : files) {
-				fileList.add(file);
-				if (file.isDirectory()) {
-					System.out.println("directory:" + file.getCanonicalPath());
-					getAllFiles(file, fileList);
-				} else {
-					System.out.println("     file:" + file.getCanonicalPath());
-				}
+	public static void getAllFiles(File dir, List<File> fileList)
+	{
+		File[] files = dir.listFiles();
+		for(File file : files)
+		{
+			fileList.add(file);
+			if(file.isDirectory())
+			{
+				getAllFiles(file, fileList);
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
-	public static void writeZipFile(File directoryToZip, String outputFileName, List<File> fileList) {
+	public static void writeZipFile(File directoryToZip, String outputFileName, List<File> fileList)
+	{
 
-		try {
+		try
+		{
+			logger.info("Creating zip file " + outputFileName);
 			FileOutputStream fos = new FileOutputStream(directoryToZip + "/" + outputFileName);
 			ZipOutputStream zos = new ZipOutputStream(fos);
 
-			for (File file : fileList) {
-				if (!file.isDirectory()) { // we only zip files, not directories
+			for(File file : fileList)
+			{
+				if(!file.isDirectory())
+				{ // we only zip files, not directories
 					addToZip(directoryToZip, file, zos);
 				}
 			}
 
 			zos.close();
 			fos.close();
-		} catch (FileNotFoundException e) {
+		}
+		catch(FileNotFoundException e)
+		{
 			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		catch(IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void addToZip(File directoryToZip, File file, ZipOutputStream zos) throws FileNotFoundException,
-			IOException {
+	public static void addToZip(File directoryToZip, File file, ZipOutputStream zos) throws FileNotFoundException, IOException
+	{
 
 		FileInputStream fis = new FileInputStream(file);
 
-		// we want the zipEntry's path to be a relative path that is relative
-		// to the directory being zipped, so chop off the rest of the path
 		String zipFilePath = file.getCanonicalPath().substring(directoryToZip.getCanonicalPath().length() + 1, file.getCanonicalPath().length());
-		System.out.println("Writing '" + zipFilePath + "' to zip file");
+		logger.info("Writing '" + zipFilePath + "' to zip file");
 		ZipEntry zipEntry = new ZipEntry(zipFilePath);
 		zos.putNextEntry(zipEntry);
 
 		byte[] bytes = new byte[1024];
 		int length;
-		while ((length = fis.read(bytes)) >= 0) {
+		while((length = fis.read(bytes)) >= 0)
+		{
 			zos.write(bytes, 0, length);
 		}
 
