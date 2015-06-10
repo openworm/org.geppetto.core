3907ddfc27743d65258d68dabe76193059651118
jrmartin <yeshuarmartinez@gmail.com>
2015-06-10 12:47:42 +0200

    return string from enum

--- src/main/java/org/geppetto/core/data/model/PersistedDataType.java
+++ src/main/java/org/geppetto/core/data/model/PersistedDataType.java
@@ -35,5 +35,23 @@ package org.geppetto.core.data.model;
 
 public enum PersistedDataType
 {
-	RECORDING, GEPPETTO_PROJECT, MODEL
+	RECORDING("RECORDING"), GEPPETTO_PROJECT("GEPPETTO_PROJECT"), MODEL("MODEL");
+	
+	private final String text;
+
+    /**
+     * @param text
+     */
+    private PersistedDataType(final String text) {
+        this.text = text;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return text;
+    }
+
 }
