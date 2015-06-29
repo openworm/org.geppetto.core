aa3863f65ad5c76cd21bdbe6e390da59e20ce663
johnidol <giovanni.idili@gmail.com>
2015-06-29 16:45:52 +0100

    converted acnet sample

---
+++ src/main/resources/projects/AuditoryCortex/script.js
@@ -0,0 +1,12 @@
+G.addWidget(6);
+Connectivity1.setName("Connectivity matrix");
+Connectivity1.setData(acnet2);
+G.addWidget(1);
+Popup1.setMessage("Network model of primary auditory cortex in rodents. David Beeman, University of Colorado, Boulder. For more information see http://opensourcebrain.org/projects/acnet2.");
+Popup1.setName("Description");
+Connectivity1.setPosition(780,182)
+G.incrementCameraPan(-0.1, 0))
+Connectivity1.setPosition(645,210)
+Popup1.setPosition(641,78)
+Popup1.setSize(110.80000019073486,665.8000001907349)
+acnet2.baskets_12_0.electrical.select()
