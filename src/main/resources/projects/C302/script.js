e67c547daabd52a05046b8f55b79e4e16e9cbbab
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-07-07 20:46:29 +0100

    added line to play results

--- src/main/resources/projects/C302/script.js
+++ src/main/resources/projects/C302/script.js
@@ -17,3 +17,5 @@ G.addBrightnessFunction(c302.BDUR_0.electrical, c302.BDUR_0.electrical.Simulatio
 G.addBrightnessFunction(c302.I1R_0.electrical, c302.I1R_0.electrical.SimulationTree.generic_iaf_cell.v, function(x){return (x+0.06)/0.01;});
 G.addBrightnessFunction(c302.I2L_0.electrical, c302.I2L_0.electrical.SimulationTree.generic_iaf_cell.v, function(x){return (x+0.06)/0.01;});
 G.addBrightnessFunction(c302.PVDR_0.electrical, c302.PVDR_0.electrical.SimulationTree.generic_iaf_cell.v, function(x){return (x+0.06)/0.01;});
+
+Project.getActiveExperiment().play({step:1});
