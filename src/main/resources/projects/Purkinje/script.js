b0876a52083cf7003240cd2534299b59dd229e31
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-07-13 16:51:26 +0100

    Sample update

---
+++ src/main/resources/projects/Purkinje/script.js
@@ -0,0 +1,9 @@
+G.addWidget(3).setData(purkinje.electrical.VisualizationTree, {expandNodes: true});
+TreeVisualiserDAT1.setPosition(147,90);
+TreeVisualiserDAT1.setSize(448,452);
+TreeVisualiserDAT1.setName("Purkinje cell");
+purkinje.electrical.VisualizationTree.ChannelDensities.Leak.show(true);
+G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription());
+Popup1.setPosition(1004,98);
+Popup1.setSize(138,407);
+Popup1.setName("Description");
