5faf52b674b074156e4046638957d3d1458a9ca5
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-12-15 21:10:06 +0000

    Check for null

--- src/main/java/org/geppetto/core/data/FindLocalProjectsVisitor.java
+++ src/main/java/org/geppetto/core/data/FindLocalProjectsVisitor.java
@@ -85,15 +85,19 @@ public class FindLocalProjectsVisitor extends SimpleFileVisitor<Path>
 			Gson gson = builder.create();
 			String projectPath = File.separator + "projects" + File.separator;
 			String localString = file.toString().substring(file.toString().indexOf(projectPath));
-			InputStream stream = DefaultGeppettoDataManager.class.getClassLoader().getResourceAsStream(localString);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
-			LocalGeppettoProject project = gson.fromJson(reader, LocalGeppettoProject.class);
-			for(IExperiment e : project.getExperiments())
+			InputStream stream = DefaultGeppettoDataManager.class.getClassLoader().getResourceAsStream(localString);
+			if(stream != null)
 			{
-				e.setParentProject(project);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+				LocalGeppettoProject project = gson.fromJson(reader, LocalGeppettoProject.class);
+				for(IExperiment e : project.getExperiments())
+				{
+					e.setParentProject(project);
+				}
+				projects.put(project.getId(), project);
 			}
-			projects.put(project.getId(), project);
 		}
 		return FileVisitResult.CONTINUE;
 	}
