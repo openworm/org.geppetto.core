c895b86802f34dc1de07b36acac9636130e5ce58
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-01-07 15:32:23 +0000

    Updating scripts

--- src/main/resources/projects/AuditoryCortex/script.js
+++ src/main/resources/projects/AuditoryCortex/script.js
@@ -9,15 +9,14 @@ G.incrementCameraPan(-0.1, 0)
 Connectivity1.setPosition(645,210)
 Popup1.setPosition(641,78)
 Popup1.setSize(110.80000019073486,665.8000001907349)
-acnet2.baskets_12_0.electrical.select()
+acnet2.baskets_12[0].select()
 
-acnet2.baskets_12_0.electrical.getSimulationTree();
 G.addWidget(0);
 Plot1.setName("Primary Auditory Cortext Network - Basket Cell - Voltage");
 options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:400,show:false}};
 Plot1.setOptions(options);
 Plot1.setPosition(113, 90);
 Plot1.setSize(230,445);
-Plot1.plotData(acnet2.baskets_12_0.electrical.SimulationTree.bask[0].v);
+Plot1.plotData(acnet2.baskets_12[0].v);
 
 Project.getActiveExperiment().play({playAll:true});
--- src/main/resources/projects/SingleComponentHH/HH_Geppetto_Script.js
+++ src/main/resources/projects/SingleComponentHH/HH_Geppetto_Script.js
@@ -4,8 +4,8 @@ options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:400,show:false}};
 Plot1.setOptions(options);
 Plot1.setPosition(120, 90);
 Plot1.setSize(230,465);
-Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].v);
-Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].spiking);
+Plot1.plotData(hhcell.hhpop[0].v);
+Plot1.plotData(hhcell.hhpop[0].spiking);
 
 G.addWidget(Widgets.PLOT);
 
@@ -15,13 +15,13 @@ Plot2.setOptions(options)
 Plot2.setName("Gating Variables");
 Plot2.setPosition(120,350);
 Plot2.setSize(285,465)
-Plot2.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q);
-Plot2.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);
-Plot2.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q);
+Plot2.plotData(hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q);
+Plot2.plotData(hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);
+Plot2.plotData(hhcell.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q);
 
-Plot2.setLegend(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q,"Sodium h.q");
-Plot2.setLegend(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q,"Sodium m.q");
-Plot2.setLegend(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q,"Potassium n.q");
+Plot2.setLegend(hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q,"Sodium h.q");
+Plot2.setLegend(hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q,"Sodium m.q");
+Plot2.setLegend(hhcell.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q,"Potassium n.q");
 
 G.addWidget(Widgets.POPUP);
 Popup1.setMessage(Project.getActiveExperiment().getDescription());
@@ -29,16 +29,7 @@ Popup1.setName("Description");
 Popup1.setPosition(870, 90);
 Popup1.setSize(160,465)
 
-
-//hhcell.electrical.getModelTree();
-//G.addWidget(Widgets.POPUP);
-//Popup2.setPosition(870,259);
-//Popup2.setSize(321.8,464.8);
-//Popup2.addCustomNodeHandler(function(node){alert(node.getName());}, 'click', 'Network');
-//Popup2.addCustomNodeHandler(function(node){alert(node.getName());}, 'dblclick', 'PulseGenerator');
-//Popup2.setMessage(hhcell.electrical.ModelTree.ModelDescription.value);
-
-G.addBrightnessFunction(hhcell.electrical, hhcell.electrical.SimulationTree.hhpop[0].v, function(x){return (x+0.07)/0.1;});
+G.addBrightnessFunction(hhcell.hhpop[0], hhcell.hhpop[0].v, function(x){return (x+0.07)/0.1;});
 
 Project.getActiveExperiment().play({step:1});
 
