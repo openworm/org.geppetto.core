d5257a515e0f934a5117d29161a042fc047609e2
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-01-13 19:28:54 +0000

    adding ca1 sample

---
+++ src/main/resources/projects/CA1/script.js
@@ -0,0 +1,9 @@
+G.addWidget(3).setData(ca1.electrical.VisualizationTree, {expandNodes: true});
+TreeVisualiserDAT1.setPosition(147,90);
+TreeVisualiserDAT1.setSize(448,452);
+TreeVisualiserDAT1.setName("");
+ca1.electrical.VisualizationTree.ChannelDensities.Density.Leak.show(true);
+G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription());
+Popup1.setPosition(1004,98);
+Popup1.setSize(138,407);
+Popup1.setName("Description");
