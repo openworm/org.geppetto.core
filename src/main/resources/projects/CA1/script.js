73e68ce17bed8e3067956cf7442b897006bb0f0a
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-01-22 18:48:01 +0000

    Removing purkinje, updating CA1

--- src/main/resources/projects/CA1/script.js
+++ src/main/resources/projects/CA1/script.js
@@ -1,9 +1,10 @@
-G.addWidget(3).setData(ca1.electrical.VisualizationTree, {expandNodes: true});
+G.addWidget(3).setData(ca1.getType(), {expandNodes: true});
 TreeVisualiserDAT1.setPosition(147,90);
 TreeVisualiserDAT1.setSize(448,452);
-TreeVisualiserDAT1.setName("");
-ca1.electrical.VisualizationTree.ChannelDensities.Density.Leak.show(true);
+TreeVisualiserDAT1.setName("Hippocampus CA1 pyramidal cell model");
+//visual groups to fix still
+//ca1.getType()..ChannelDensities.Density.Leak.show(true);
 G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription());
-Popup1.setPosition(1004,98);
-Popup1.setSize(138,407);
+Popup1.setPosition(950,98);
+Popup1.setSize(138,500);
 Popup1.setName("Description");
