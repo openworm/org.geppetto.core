8e7d833d4261a7b94090e825e7691357cadd3a66
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-02-02 14:32:12 +0000

    setting expandnodes to false otherwise it is too slow

--- src/main/resources/projects/CA1/script.js
+++ src/main/resources/projects/CA1/script.js
@@ -1,4 +1,4 @@
-G.addWidget(3).setData(ca1.getType(), {expandNodes: true});
+G.addWidget(3).setData(ca1.getType());
 TreeVisualiserDAT1.setPosition(147,90);
 TreeVisualiserDAT1.setSize(448,452);
 TreeVisualiserDAT1.setName("Hippocampus CA1 pyramidal cell model");
