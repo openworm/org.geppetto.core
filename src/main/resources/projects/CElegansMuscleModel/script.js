ae8cbd98c944bca07738fe58ef6b7dbb43eb2f19
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-02-10 20:35:13 +0000

    some work on muscle sample

--- src/main/resources/projects/CElegansMuscleModel/script.js
+++ src/main/resources/projects/CElegansMuscleModel/script.js
@@ -3,8 +3,8 @@ G.addWidget(GEPPETTO.Widgets.PLOT);
 Plot1.setName("Voltage response for neuron and muscle (mV)");
 Plot1.setSize(256,453);
 Plot1.setPosition(184,83);
-Plot1.plotData(net1.muscle[0].soma_0.v,options);
-Plot1.plotData(net1.neuron[0].soma_0.v,options);
+Plot1.plotData(net1.muscle[0].v,options);
+Plot1.plotData(net1.neuron[0].v,options);
 
 options = {yaxis:{min:-0.000005,max:0.000055},xaxis:{min:0,max:400,show:false}};
 G.addWidget(GEPPETTO.Widgets.PLOT);
