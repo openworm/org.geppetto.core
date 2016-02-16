00dfa7f6e65c355b70d7d2cab855dc6f12de8245
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-02-16 19:53:21 +0000

    Sample improvement, restored VG.

--- src/main/resources/projects/CA1/script.js
+++ src/main/resources/projects/CA1/script.js
@@ -2,8 +2,8 @@ G.addWidget(3).setData(ca1.getType());
 TreeVisualiserDAT1.setPosition(147,90);
 TreeVisualiserDAT1.setSize(448,452);
 TreeVisualiserDAT1.setName("Hippocampus CA1 pyramidal cell model");
-//visual groups to fix still
-//ca1.getType()..ChannelDensities.Density.Leak.show(true);
+
+Model.neuroml.morphology_CA1.kad_dendrite_group.show(true)
 G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription());
 Popup1.setPosition(950,98);
 Popup1.setSize(138,500);
