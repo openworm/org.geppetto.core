5bc9c4c3a9d6aab69b855eab48aa9f318587fb54
johnidol <giovanni.idili@gmail.com>
2016-04-14 09:42:03 +0100

    added VFB sample project

---
+++ src/main/resources/projects/VFB/script.js
@@ -0,0 +1,7 @@
+G.addWidget(Widgets.POPUP);
+Popup1.setName("VFB Sample");
+Popup1.setMessage(Project.getActiveExperiment().getDescription());
+Popup1.setSize(172.80000019073486,394.80000019073486)
+Popup1.setPosition(129,77)
+G.incrementCameraZoom(-0.16)
+G.incrementCameraRotate(0, 0, 0.07)
