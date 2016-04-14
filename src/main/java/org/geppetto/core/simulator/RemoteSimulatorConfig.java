fa49aea5ac266d210c07e73de8e2669182d50211
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-04-14 13:17:27 +0100

    adding remote simulator config

--- src/main/java/org/geppetto/core/simulator/RemoteSimulatorConfig.java
+++ src/main/java/org/geppetto/core/simulator/RemoteSimulatorConfig.java
@@ -32,9 +32,49 @@
  *******************************************************************************/
 package org.geppetto.core.simulator;
 
+import java.util.Map;
+
 public class RemoteSimulatorConfig extends ExternalSimulatorConfig
 {
 
+	private String username;
+	private String password;
+	private String url;
+	private Map<String ,String> simulatorParameters;
+	
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	public String getUrl()
+	{
+		return url;
+	}
+	public void setUrl(String url)
+	{
+		this.url = url;
+	}
+	public Map<String, String> getSimulatorParameters()
+	{
+		return simulatorParameters;
+	}
+	public void setSimulatorParameters(Map<String, String> simulatorParameters)
+	{
+		this.simulatorParameters = simulatorParameters;
+	}
 	
 
 }
