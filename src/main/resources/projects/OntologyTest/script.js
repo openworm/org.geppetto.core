1514714abdc972483affe982b2ee46aa06f19435
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-04-19 12:09:47 +0100

    Script update

--- src/main/resources/projects/OntologyTest/script.js
+++ src/main/resources/projects/OntologyTest/script.js
@@ -4,7 +4,9 @@ Model.getDatasources()[0].fetchVariable('FBbt_00100219')
 Instances.getInstance("VFB_00000001.VFB_00000001_meta")
 Instances.getInstance("FBbt_00100219.FBbt_00100219_meta")
 
-G.addWidget(1).setData(FBbt_00100219.FBbt_00100219_meta)
-G.addWidget(1).setData(VFB_00000001.VFB_00000001_meta)
+G.addWidget(1).setData(FBbt_00100219.FBbt_00100219_meta).setName(FBbt_00100219.getName())
+G.addWidget(1).setData(VFB_00000001.VFB_00000001_meta).setName(VFB_00000001.getName())
 
 window.Model.getLibraries()[1].getTypes()[0].resolve()
+Popup2.setPosition(87,110)
+Popup1.setPosition(587,110)
