dceec335a5898f24a4e140555a0d5e96d4e5ec2e
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-04-25 17:36:53 +0100

    Position fix

--- src/main/resources/projects/OntologyTest/script.js
+++ src/main/resources/projects/OntologyTest/script.js
@@ -6,7 +6,7 @@ Instances.getInstance("FBbt_00100219.FBbt_00100219_meta")
 
 var customHandler=function(node, path){ var n;try {n = eval(path);} catch (ex) {node = undefined;}var meta=path+"."+path+"_meta"; if(n!=undefined){var metanode= Instances.getInstance(meta);G.addWidget(1).setData(metanode).setName(n.getName()).addCustomNodeHandler(customHandler,'click');}else{Model.getDatasources()[0].fetchVariable(path,function(){Instances.getInstance(meta);G.addWidget(1).setData(eval(meta)).setName(eval(path).getName()).addCustomNodeHandler(customHandler,'click');});}};
 
-G.addWidget(1).setData(FBbt_00100219.FBbt_00100219_meta).setName(FBbt_00100219.getName()).setPosition(587,110).addCustomNodeHandler(customHandler,'click');
+G.addWidget(1).setData(FBbt_00100219.FBbt_00100219_meta).setName(FBbt_00100219.getName()).setPosition(92,110).addCustomNodeHandler(customHandler,'click');
 G.addWidget(1).setData(VFB_00000001.VFB_00000001_meta).setName(VFB_00000001.getName()).setPosition(587,110).addCustomNodeHandler(customHandler,'click');
 
 window.Model.getLibraries()[1].getTypes()[0].resolve()
