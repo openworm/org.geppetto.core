d343ff7dde6256c097ab8eec1bdcd00004edfc1e
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-05-26 16:45:17 +0100

    NWB Sample update

--- src/main/resources/projects/NWBSample/script.js
+++ src/main/resources/projects/NWBSample/script.js
@@ -1,7 +1,9 @@
 Instances.getInstance("nwbSample.stimulus")
 Instances.getInstance("nwbSample.response")
+Instances.getInstance("nwbSample.metadata")
 Instances.getInstance("nwbSample.time")
-G.addWidget(0).plotXYData(nwbSample.response,nwbSample.time).plotXYData(nwbSample.stimulus,nwbSample.time)
-G.addWidget(0).plotXYData(nwbSample.response,nwbSample.time)
-Plot2.setSize(390.8,564.8).setPosition(514,138).setName("Response")
-Plot1.setPosition(140,82).setName("Stimulus and Response")
+G.addWidget(0).plotXYData(nwbSample.response,nwbSample.time).plotXYData(nwbSample.stimulus,nwbSample.time).setSize(273.8,556.8).setPosition(136,69).setName("Stimulus and response")
+G.addWidget(0).plotXYData(nwbSample.response,nwbSample.time).setSize(385.8,557.8).setPosition(134,367).setName("Response")
+
+G.addWidget(1).setData(nwbSample.metadata).setName('Metadata').setPosition(713,228).setSize(525.8,580.8);
+G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription()).setSize(150,581.8).setPosition(713,68.5).setName("Description")
