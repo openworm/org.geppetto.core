25c383217af19fd6ba840e01423f0e142f0f5da7
Nitesh Thali <niteshthali08@gmail.com>
2016-08-30 13:56:33 -0700

    Added functionality to plot graphs on callback

--- src/main/resources/projects/NWBSample/script.js
+++ src/main/resources/projects/NWBSample/script.js
@@ -1,17 +1,13 @@
-Instances.getInstance("nwbSample.stimulus");
-Instances.getInstance("nwbSample.response");
 Instances.getInstance("nwbSample.metadata");
 Instances.getInstance("nwbSample.time");
-G.addWidget(0).plotXYData(nwbSample.response,nwbSample.time).plotXYData(nwbSample.stimulus,nwbSample.time).setSize(273.8,556.8).setPosition(136,69).setName("Stimulus and response");
-G.addWidget(0).plotXYData(nwbSample.response,nwbSample.time).setSize(385.8,557.8).setPosition(134,367).setName("Response");
+
 G.addWidget(1).setData(nwbSample.metadata).setName('Metadata').setPosition(713,228).setSize(525.8,580.8);
 G.addWidget(1).setMessage(Project.getActiveExperiment().getDescription()).setSize(150,581.8).setPosition(713,68.5).setName("Description");
 
-
-GEPPETTO.ControlPanel.setColumnMeta([{"columnName": "path","order": 1,"locked": false,"displayName": "Path","source": "$entity$.getPath()"},{"columnName": "sweep", "order": 2, "locked": false, "displayName": "Sweep No.", "source": "$entity$.getName()" }, { "columnName": "name", "order": 3, "locked": false, "displayName": "Name", "source": "$entity$.getVariables()[1].getType().getVariables()[0].getValue().getWrappedObj()['value']['text']" }, { "columnName": "amplitude", "order": 4, "locked": false, "displayName": "Amplitude", "source": "$entity$.getVariables()[1].getType().getVariables()[1].getValue().getWrappedObj()['value']['text']" }, { "columnName": "controls", "order": 4, "locked": false, "customComponent": GEPPETTO.ControlsComponent, "displayName": "Controls", "source": "", "action": "GEPPETTO.FE.refresh();" }]);
-GEPPETTO.ControlPanel.setColumns(['sweep', 'name', 'amplitude', 'controls']);
+GEPPETTO.ControlPanel.setColumnMeta([{ "columnName": "path", "order": 1, "locked": false, "displayName": "Path", "source": "$entity$.getPath()" }, { "columnName": "sweep", "order": 2, "locked": false, "displayName": "Sweep No.", "source": "$entity$.getName()" }, { "columnName": "name", "order": 3, "locked": false, "displayName": "Name", "source": "$entity$.getVariables()[1].getType().getVariables()[0].getValue().getWrappedObj()['value']['text']" }, { "columnName": "amplitude", "order": 4, "locked": false, "displayName": "Amplitude", "source": "$entity$.getVariables()[1].getType().getVariables()[1].getValue().getWrappedObj()['value']['text']" }, { "columnName": "interval", "order": 5, "locked": false, "displayName": "Interval", "source": "$entity$.getVariables()[1].getType().getVariables()[2].getValue().getWrappedObj()['value']['text']" }, { "columnName": "controls", "order": 6, "locked": false, "customComponent": GEPPETTO.ControlsComponent, "displayName": "Controls", "source": "", "action": "GEPPETTO.FE.refresh();" }]);
+GEPPETTO.ControlPanel.setColumns(['sweep', 'name', 'amplitude', 'interval','controls']);
 GEPPETTO.ControlPanel.setDataFilter(function(entities) { var compositeInstances = GEPPETTO.ModelFactory.getAllTypesOfMetaType(GEPPETTO.Resources.COMPOSITE_TYPE_NODE, entities); var sweepInstances = []; for (index in compositeInstances) { allVariables = compositeInstances[index].getVariables(); for(v in allVariables){ if(allVariables[v].getId().startsWith('Sweep_')){ s_r = allVariables[v].getType().getVariables(); sweepInstances.push(s_r[0].getType()); sweepInstances.push(s_r[1].getType()); } } } return sweepInstances; }); 
-GEPPETTO.ControlPanel.setControlsConfig({ "VisualCapability": {}, "Common": { "plot": { "id": "plot", "actions": ["$instance$.getVariables()[0].getValue().resolve()"], "icon": "fa-eye", "label": "Plot", "tooltip": "Plot Sweep" } } }); 
+GEPPETTO.ControlPanel.setControlsConfig({ "VisualCapability": {}, "Common": { "plot": { "id": "plot", "actions": ["$instance$.getVariables()[0].getValue().resolve(function(){ var instancePath = ''; for (var i = 0; i < GEPPETTO.ModelFactory.allPaths.length; i++) { if (GEPPETTO.ModelFactory.allPaths[i].type == $instance$.getPath()) { instancePath = GEPPETTO.ModelFactory.allPaths[i].path + '.recording'; break; } } G.addWidget(0).plotXYData(Instances.getInstance(instancePath), nwbSample.time).setSize(273.8,556.8).setPosition(136,69); }); "], "icon": "fa-eye", "label": "Plot", "tooltip": "Plot Sweep" } } });
 GEPPETTO.ControlPanel.setControls({ "VisualCapability": [], "Common": ['plot'] });
 
 GEPPETTO.ControlPanel.addData(Instances);
