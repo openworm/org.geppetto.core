3245a4f320a870db406db5c49c15b702ef494dcc
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-10-19 10:59:48 +0100

    Removing Ontology sample

--- src/main/resources/projects/OntologyTest/script.js
+++
@@ -1,7 +0,0 @@
-Model.getDatasources()[0].fetchVariable('VFB_00000001');
-Model.getDatasources()[0].fetchVariable('FBbt_00100219');
-
-Instances.getInstance("VFB_00000001.VFB_00000001_meta");
-Instances.getInstance("FBbt_00100219.FBbt_00100219_meta");
-
-window.Model.getLibraries()[1].getTypes()[0].resolve();
