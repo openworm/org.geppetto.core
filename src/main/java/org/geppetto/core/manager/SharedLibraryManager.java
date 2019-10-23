5a15aeb2d47cb390f2839c7ec46b9e99ac09add8
tarelli <matteo.cantarelli@gmail.com>
2019-10-23 13:22:04 +0100

    Model bundle released -> v1.1.0

--- src/main/java/org/geppetto/core/manager/SharedLibraryManager.java
+++ src/main/java/org/geppetto/core/manager/SharedLibraryManager.java
@@ -48,7 +48,7 @@ public class SharedLibraryManager
 			GeppettoPackage.Registry.INSTANCE.put(GeppettoPackage.eNS_URI, GeppettoPackage.eINSTANCE);
 
 			// We add all supported versions of the schema
-			String[] versions = new String[] { "master", "development", "feature/28" };
+			String[] versions = new String[] { "master", "development"};
 			for(String version : versions)
 			{
 				GeppettoPackage.Registry.INSTANCE.put(GeppettoPackage.eNS_URI_TEMPLATE.replace("$VERSION$", version), GeppettoPackage.eINSTANCE);
