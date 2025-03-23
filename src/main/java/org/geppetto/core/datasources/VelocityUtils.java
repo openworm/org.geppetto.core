ea56bf6c2a3f541403aec66b8b65f6bfb937f9da
Rob Court <robbie.court@gmail.com>
2025-03-23 04:31:51 +0000

    Revert "Limit Velocity iterations to prevent infinite loops and log available context keys during processing"
    This reverts commit 618349c1f28bffcf6043e3c7c7de9127d256724b.

--- src/main/java/org/geppetto/core/datasources/VelocityUtils.java
+++ src/main/java/org/geppetto/core/datasources/VelocityUtils.java
@@ -56,7 +56,7 @@ public class VelocityUtils
 			int iterations = 0;
 			
 			// In this loop we keep using velocity until all the replacements are done
-			while(result.contains("$") && iterations < 5)
+			while(result.contains("$") && !result.equals(previousResult))
 			{
 				iterations++;
 				previousResult = result;
@@ -78,7 +78,6 @@ public class VelocityUtils
 						// Find next $ after current position
 						index = result.indexOf("$", index + 1);
 					}
-					System.out.println("Available context keys: " + context.getKeys());
 				}
 				
 				writer.close();
